993eca7d4ddf7127a01ebf66201fd3297e99ced4
João Paulo Martins <joao_paulo_@outlook.com>
2018-08-23 02:41:00 -0300

    Add check if Vundle is installer

--- install.sh
+++ install.sh
@@ -11,8 +11,12 @@ echo "> Installing colorscheme"
 wget -q -O ~/.config/nvim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
 
 # Install Vundle.vim
-echo "> Installing Vundle.vim"
-git clone -q https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
+if [ -d ~/.config/nvim/bundle/Vundle.vim ]; then
+  echo "> Vundle.vim already installed"
+else
+  echo "> Installing Vundle.vim"
+  git clone -q https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
+fi
 
 # Download plugins
 echo "> Installing Vim Plugins"
